6ed03fe60d27f83f8991233867c622bf19050cee
Aeron01 <one.soulreaper@gmail.com>
2021-02-17 10:04:25 +0100

    minor update

--- trading.js
+++ trading.js
@@ -1,7 +1,7 @@
-let btc = 5000
-let btcDwn = 20
-let btcUp = 40000
-let btcSty = 8000
+let btc = 20000 // prix du btc
+let btcDwn = 10000 // prix du btc pour acheter
+let btcUp = 40000  // prix du btc pour vendre
+let btcSty = 18000 // somme en btc que l'on posséde
 if (btc < btcDwn || btc < btcSty) {
   console.log('Btc is ' + `${btc}` + ' €. Buy as much as you can before it s too late !')
 } else if (btc > btcSty || btc > btcUp) {
